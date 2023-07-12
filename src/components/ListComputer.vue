2105f51b1d13c43fff672e6ee1cc92f698a1378e
tobias santos <tobiassantospb@gmail.com>
2023-07-12 16:49:03 -0300

    2 commit

--- src/components/ListComputer.vue
+++ src/components/ListComputer.vue
@@ -3,14 +3,14 @@ export default {
     name: "lista",
     data() {
         return{
-            pokemons: [{}]
+            computadores: [{}]
         }
     },
     methods: {
 
     },
     mounted() {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        fetch('http://localhost:8080/Produto')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Erro ao fazer a requisição da API.');
@@ -18,8 +18,8 @@ export default {
             return response.json();
         })
         .then(data => {
-            this.pokemons = data.results
-            console.log(this.pokemons);
+            this.computadores = data
+            // console.log(this.computadores);
         })
         .catch(error => {
             console.error(error);
@@ -31,9 +31,25 @@ export default {
 </script>
 
 <template>
+    <h1>Todos os nossos computadores</h1>
     <div class="lista">
-        <div class="computador" v-for="pokemon in pokemons" :key="pokemon.index"> {{pokemon.name}} </div>
+        <div class="computador" v-for="comp in computadores" :key="comp.index">
+          <strong>Produto:</strong> {{ comp.nome_produto }} <br>
+          <strong>Descricao:</strong> {{ comp.descricao }} <br>
+          <strong>Preco:</strong> {{ comp.preco }} <br>
+        </div>
     </div>
+
+    <a href="/cadastrar">
+      <button class="btnCad">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
+        </svg>
+        <!-- <svg class="voltar" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path d="M9.4 233.4c-12.5 12.5-12.5   32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
+        </svg> -->
+      </button>
+    </a>
 </template>
 
 
@@ -45,9 +61,33 @@ export default {
 
 }
 .computador{
-    border: 1px solid #f00;
+    border: 1px dashed #f00;
     margin: 10px;
-    width: 100px;
-    height: 30px;
+    width: auto;
+    padding: 10px;
+    height: auto;
+    outline: 1px solid #f00;
+    outline-offset: 2px;
+    /* box-shadow: 0 0 7px #f00; */
+}
+.btnCad{
+  display: block;
+  position: fixed;
+  right: 1.3rem;
+  bottom: 1.3rem;
+  width: 51px;
+  height: 51px;
+  background-color: #f00;
+  border-radius: 50%;
+  border: 0.5px solid #f00;
+  box-shadow: 0 0 15px #f00;
+}
+.btnCad svg{
+  width: 40px;
+  height: 40px;
+  margin: -15px;
+}
+strong{
+  font-weight: bold;
 }
 </style>
